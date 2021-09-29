e2c85bf8181f3a49066766ddfda273ec000cf4aa
Mamurov <akylmamurov1@gmail.com>
2021-09-29 20:11:04 +0600

    first commit

---
+++ task.py
@@ -0,0 +1,58 @@
+
+"""
+Write a program that repeatedly asks the user to enter product names and prices.
+Store all this in a dictionary, the keys of which are the names of products, and the values are prices. 
+When the user finishes entering products and prices, allow him to re-enter the product name and print the corresponding price or message if the product is not in the dictionary.
+"""
+
+# prod = {}
+# for i in range(5):
+#     p = input()
+#     ts = int(int())
+#     prod[p]=ts
+# print(prod)
+# pr = input()
+# if pr in prod:
+#     print(prod[pr])
+# else:
+#     print('К сожелению нет')
+
+# prod = {'apple': 100, 'orange': 120, 'Lemon': 60, 'tangerine': 50} 
+
+"""
+Using the dictionary created in the previous task, allow the user to enter the dollar amount and print out all the products,
+the price of which is less than this amount.
+"""
+
+# while True:
+#     passwordOne = input("password 1:")
+#     passwordTwo = input("password 2:")
+#     a = True
+#     if len(passwordOne)<8:
+#         a = False
+#         print("short")
+#     if passwordOne != passwordTwo:
+#         a = False
+#         print("They differ")
+#     if "123" in passwordOne:
+#         a = False
+#         print("Plain")
+#     if a:
+#         break
+# print("OK")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
